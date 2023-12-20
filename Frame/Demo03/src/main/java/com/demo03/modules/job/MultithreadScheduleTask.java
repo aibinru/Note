4e3c4996299fa44489ee3c91c74563dfe05a8f3a
aibinru <am19136754034@gamil.com>
2023-12-20 17:54:56 +0800

    图片上传demo

--- Frame/Demo03/src/main/java/com/demo03/modules/job/MultithreadScheduleTask.java
+++ Frame/Demo03/src/main/java/com/demo03/modules/job/MultithreadScheduleTask.java
@@ -11,16 +11,16 @@ import java.time.LocalDateTime;
 @EnableAsync
 public class MultithreadScheduleTask {
 
-    @Async
-    @Scheduled(fixedDelay = 1000)
+    //@Async
+    //@Scheduled(fixedDelay = 1000)
     public void first() throws InterruptedException {
         System.out.println("第一个定时任务开始："+ LocalDateTime.now().toLocalTime()+"\r\n线程："+Thread.currentThread().getName());
         System.out.println();
         Thread.sleep(1000*10);
     }
 
-    @Async
-    @Scheduled(fixedDelay = 2000)
+    //@Async
+    //@Scheduled(fixedDelay = 2000)
     public void second(){
         System.out.println("第二个定时任务开始："+LocalDateTime.now().toLocalTime()+"\r\n线程："+Thread.currentThread().getName());
         System.out.println();
